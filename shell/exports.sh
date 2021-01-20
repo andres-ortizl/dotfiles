d674f8246a8393de6a8e21017e54c19db65b1ec8
Andrés Ortíz <andres.ortiz@clarity.ai>
2021-01-20 11:33:02 +0100

    dotly framework port

---
+++ shell/exports.sh
@@ -0,0 +1,30 @@
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
+export GEM_HOME="$HOME/.gem"
+export GOPATH="$HOME/.go"
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+
+export FZF_DEFAULT_OPTS='
+  --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
+  --reverse
+'
+
+export path=(
+  "$HOME/bin"
+  "$DOTLY_PATH/bin"
+  "$DOTFILES_PATH/bin"
+  "$JAVA_HOME/bin"
+  "$GEM_HOME/bin"
+  "$GOPATH/bin"
+  "$HOME/.cargo/bin"
+  "/usr/local/opt/ruby/bin"
+  "/usr/local/opt/python/libexec/bin"
+  "/usr/local/bin"
+  "/usr/local/sbin"
+  "/bin"
+  "/usr/bin"
+  "/usr/sbin"
+  "/sbin"
+  "/usr/local/opt/python@3.7/bin"
+  "/Users/andrew/Library/Python/3.7/lib/python/site-packages"
+)
