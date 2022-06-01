99adac5e4ef2e2cd6538446564df5260637d2dd8
Andrés Ortiz <andres.ortiz@clarity.ai>
2022-06-01 14:34:52 +0200

    update macos scripts

--- os/mac/install.sh
+++ os/mac/install.sh
@@ -1,1 +1,8 @@
-# tbd
+# Download HomeBrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Restore packages from Brewfile
+brew bundle --file="$MYPYDOTFILES/os/mac/Brewfile" --force cleanup
+
+# Setting defaults for mac
+$MYPYDOTFILES/os/mac/defaults.sh
--- shell/functions.sh
+++ shell/functions.sh
@@ -98,3 +98,21 @@ else
   echo "Docker daemon is not running! (ಠ_ಠ)"
 fi
 }
+
+function fh() {
+  eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
+
+function ch() {
+  local cols sep
+  cols=$(( COLUMNS / 3 ))
+  sep='{::}'
+
+  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+
+  sqlite3 -separator $sep /tmp/h \
+    "select substr(title, 1, $cols), url
+     from urls order by last_visit_time desc" |
+  awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+  fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+}
