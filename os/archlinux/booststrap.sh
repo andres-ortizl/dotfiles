7d01433602ad372112e690b756f2f71c60145cfe
Andrés <andres.ortiz.xyz@gmail.com>
2022-12-10 03:04:45 +0100

    Update booststrap.sh

--- os/archlinux/booststrap.sh
+++ os/archlinux/booststrap.sh
@@ -1,50 +1,38 @@
+#!/bin/bash
+
+# basic user
 useradd -m andrew
 passwd andrew
 usermod -aG wheel andrew
-pacman -S sudo vim nano git kitty bat lsd zsh fzf firefox choose docker docker-compose kubectl
 echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
-
-# install zimrc
-
-sudo usermod -a -G docker andrew
-sudo systemctl enable docker
-sudo systemctl start docker
-# install snapd
-git clone https://aur.archlinux.org/snapd.git
-cd snapd
-makepkg -si
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-
-snap install spotify
-snap install discord
-snap install slack
-snap install code --classic
-
-
-
 #install paru repositories
-mkdir -p /home/andrew/Desktop/repositories/paru && cd !$ && \
+mkdir -p /home/andrew/code/paru && cd !$ && \
 git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin
 makepkg -si
 
 # install blackarch repositories
-mkdir -p /home/andrew/Desktop/repositories/blackarch && \
-cd /home/andrew/Desktop/respositories/blackarch && \
+mkdir -p /home/andrew/code/blackarch && \
+cd /home/andrew/code/blackarch && \
 curl -O https://blackarch.org/strap.sh && \
 chmod +x strap.sh
 sudo su && \
-./strap .sh\
-
-
-# install theme
-# https://github.com/rxyhn/dotfiles
-paru -S awesome-git
+./strap .sh
 
 
 
-systemctl --user enable mpd.service
-systemctl --user start mpd.service
+pacman -S sudo vim nano git kitty bat lsd zsh fzf firefox choose docker docker-compose kubectl make \
+pacman-contrib stow cbatticon pamixer qtile pcmanfm alacritty redshift scrot
 
+paru -S jetbrains-toolbox discord spotify neovim picom-git bison \
+rofi-git bspwm-git qtile-git qtile-extras-git python2-iwscan python-dbus-next-git nitrogen-git
+paru -S gobuster sqlmap nmap john-git responder
+
+# install zimrc
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
+# Configure docker
+sudo usermod -a -G docker andrew
+sudo systemctl enable docker
+sudo systemctl start docker
