4c3f5a7003a6bc47fb42eecec3e8456efb9b901b
andresortiz <andres.ortiz.xyz@gmail.com>
2025-11-07 11:44:38 +0100

    🎨 (dotfiles): enhance dotfiles with new configurations, improved documentation, and remove deprecated files for better organization and usability
    🔥 (remove unused files): delete obsolete Rofi theme files, notification test script, and unused screenshots to clean up the project structure.

--- config/wlogout/init.sh
+++ config/wlogout/init.sh
@@ -3,8 +3,17 @@
 SHOT=/tmp/shot.png
 BLURRED=/tmp/shot_blurred.png
 
+
 grim -t png "$SHOT"
-convert "$SHOT" -blur 0x4 "$BLURRED" &
+magick "$SHOT" -blur 0x8 "$BLURRED" &
+
 wlogout --layout "$HOME/.config/wlogout/layout" \
         --css "$HOME/.config/wlogout/style.css" \
-        -b 4 --protocol layer-shell
+        --buttons-per-row 3 \
+        --column-spacing 50 \
+        --row-spacing 50 \
+        --margin-top 300 \
+        --margin-bottom 300 \
+        --margin-left 400 \
+        --margin-right 400 \
+        --protocol layer-shell
