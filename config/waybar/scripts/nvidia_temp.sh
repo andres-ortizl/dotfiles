c12fefec1e17cabd610f54bce4203a383308667f
andresortiz <andres.ortiz.xyz@gmail.com>
2025-11-07 11:44:38 +0100

    🎨 (dotfiles): enhance dotfiles with new configurations, improved documentation, and remove deprecated files for better organization and usability
    🔥 (remove unused files): delete obsolete Rofi theme files, notification test script, and unused screenshots to clean up the project structure.

--- config/waybar/scripts/nvidia_temp.sh
+++ config/waybar/scripts/nvidia_temp.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# Get the GPU temperature from nvidia-smi
 GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
 
-# Determine the icon based on GPU temperature
 case $GPU_TEMP in
     [0-3][0-9]) ICON="󰝦" ;;   # 0-39°C
     [4-5][0-9]) ICON="󰪞" ;;   # 40-59°C
-    [6-7][0-9]) ICON="󰪟" ;;   # 60-79°C
-    8[0-9]) ICON="󰪣" ;;       # 80-89°C
-    9[0-9]) ICON="󰪤" ;;       # 90-99°C
-    1[0-9][0-9]) ICON="󰪥" ;;  # 100-199°C (unlikely, but for safety)
-    *) ICON="󰪠" ;;             # Default (any unexpected value)
+    [6-6][0-9]) ICON="󰪟" ;;   # 60-69°C
+    [7-7][0-9]) ICON="󰪣" ;;   # 70-79°C
+    [8-8][0-9]) ICON="󰪤" ;;   # 80-89°C
+    [9-9][0-9]) ICON="󰪥" ;;   # 90-99°C
+    *) ICON="󰪦" ;;             # Default (any unexpected value)
 esac
 
-# Output JSON format for Waybar
-echo "{\"text\": \"$ICON $GPU_TEMP°C\", \"tooltip\": \"$GPU_TEMP°C\", \"icon\": \"$ICON\"}"
+echo "{\"text\": \"$ICON $GPU_TEMP°C\", \"tooltip\": \"GPU Temperature: $GPU_TEMP°C\"}"
