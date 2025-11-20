34a31a3b30bfa98dfc964aeb930ca02ef9236c1f
andresortiz <andres.ortiz.xyz@gmail.com>
2025-11-20 18:56:35 +0100

    ✨ (mac-conf.yml, zshrc_mac, amethyst.yml, atuin config, zed keymap): update configuration files to enhance user experience and streamline setup 🔧 (setup.sh): add macOS setup script to automate system preferences adjustments 🔥 (zed keymap.json): remove old keymap file and replace with mac-specific keymap 💄 (zed settings_mac.json): add mac-specific settings for Zed editor to improve usability and appearance 📝 (atuin config.toml): rename theme in Atuin configuration for clarity and consistency

---
+++ os/mac/setup.sh
@@ -0,0 +1,4 @@
+defaults write com.apple.Dock autohide-time-modifier -float 0.15
+defaults write com.apple.Dock autohide-delay -float 0
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write com.apple.universalaccess reduceTransparency -bool true
